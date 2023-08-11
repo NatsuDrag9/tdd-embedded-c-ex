c19d15e09c45e7c0df16a0cfb8158aeedbcc145f
NatsuDrag9 <rohitimandi9@zoho.com>
2023-08-11 19:06:45 +0530

    Added dockerfile and its bash scripts to build using docker engine

---
+++ ex-led-scheduler/install-cpputest-starter-project.sh
@@ -0,0 +1,32 @@
+# Clone the cpputest-starter-project repository
+git clone https://github.com/jwgrenning/cpputest-starter-project.git
+
+CPPUTEST_DIR=cpputest-starter-project
+
+# Change to the repo
+cd ${CPPUTEST_DIR}/
+
+# Set the path for CPPUTEST_HOME variable
+export CPPUTEST_HOME=../cpputest
+
+# Remove all test files from the repo to run the led-scheduler test files
+rm makefile
+
+cd tests/
+rm *.cpp
+
+# Change Docker's working directory
+cd ..
+cd ..
+
+# Move the test.cpp files to ${CPPUTEST_DIR}/tests
+mv ./*.cpp ${CPPUTEST_DIR}/tests/
+
+# Move the makefile to ${CPPUTEST_DIR}/
+mv ./makefile ${CPPUTEST_DIR}/
+
+# Change to ${CPPUTEST_DIR}/
+cd ${CPPUTEST_DIR}/
+
+# Run the makefile
+# make
