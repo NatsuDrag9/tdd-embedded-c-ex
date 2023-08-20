8a394809cebb434a4dfb142af1e5e0b5387d350c
NatsuDrag9 <rohitimandi9@zoho.com>
2023-08-20 15:52:39 +0200

    Added dockerfile and necessary bash scripts

---
+++ ex-alarm-clock-service/install-cpputest-starter-project.sh
@@ -0,0 +1,32 @@
+# Clone the cpputest-starter-project repository
+git clone https://github.com/jwgrenning/cpputest-starter-project.git unit-tests
+
+UNITTEST_DIR=unit-tests
+
+# Change to the repo
+cd ${UNITTEST_DIR}/
+
+# Set the path for CPPUTEST_HOME variable
+export CPPUTEST_HOME=../cpputest
+
+# Remove all test files from the repo to run the led-scheduler test files
+rm makefile
+
+cd tests/
+rm *.cpp
+
+# Change Docker's working directory
+cd ..
+cd ..
+
+# Move the test.cpp files to ${UNITTEST_DIR}/tests
+mv ./*.cpp ${UNITTEST_DIR}/tests/
+
+# Move the makefile to ${UNITTEST_DIR}/
+mv ./makefile ${UNITTEST_DIR}/
+
+# Change to ${UNITTEST_DIR}/
+cd ${UNITTEST_DIR}/
+
+# Run the makefile
+# make
