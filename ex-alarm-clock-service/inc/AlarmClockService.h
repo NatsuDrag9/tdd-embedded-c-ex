d1fc3a984cbc3301c4a7d763066914baabba1bdf
NatsuDrag9 <rohitimandi9@zoho.com>
2023-08-25 16:35:11 +0200

    Added macros for successful calls to callback

--- ex-alarm-clock-service/inc/AlarmClockService.h
+++ ex-alarm-clock-service/inc/AlarmClockService.h
@@ -1,9 +1,24 @@
 #ifndef D_Alarm_Clock_Service_H
 #define D_Alarm_Clock_Service_H
 
-/* Interface variables */
+#include "TimeService.h"
+
+/* Interface variables and macros */
+#define CALL_FAILED -1
+#define CALLED_WAKEUP_AFTER_SECOND 3
+#define CALLED_WAKEUP_AFTER_MINUTE 4
+#define CALLED_WAKEUP_AFTER_HOUR 5
+#define CALLED_WAKEUP_AFTER_DAY 6
+#define CALLED_WAKEUP_AFTER_MONTH 7
+#define CALLED_WAKEUP_AFTER_YEAR 8
 
 /* Function prototypes */
+int AlarmClockService_RunCallback(unsigned long ticksOfHundredms);
 void AlarmClockService_WakeupAfterSecond(void);
+void AlarmClockService_WakeupAfterMinute(void);
+void AlarmClockService_WakeupAfterHour(void);
+void AlarmClockService_WakeupAfterDay(void);
+void AlarmClockService_WakeupAfterMonth(void);
+void AlarmClockService_WakeupAfterYear(void);
 
 #endif /* D_Alarm_Clock_Service_H */
