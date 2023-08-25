7b39e6285ebdbced46ab59b838cb3b1ebefaf0af
NatsuDrag9 <rohitimandi9@zoho.com>
2023-08-25 12:34:36 +0200

    Added unit tests for time service and faketime service

--- ex-alarm-clock-service/ex-unit-tests/FakeTimeServiceTest.cpp
+++ ex-alarm-clock-service/ex-unit-tests/FakeTimeServiceTest.cpp
@@ -2,5 +2,25 @@
 
 extern "C"{
     #include "../inc/FakeTimeService.h"
+    #include "../inc/AlarmClockService.h"
 }
 
+
+TEST_GROUP(FakeTimeService){
+    void setup(){
+        TimeService_Create();
+    }
+
+    void teardown(){
+        TimerService_Destroy();
+    }
+};
+
+
+TEST(FakeTimeService, Create){
+    Time time;
+    TimeService_GetTime(&time);
+
+    LONGS_EQUAL(TIME_UNKNOWN, time.minuteOfDay);
+    LONGS_EQUAL(TIME_UNKNOWN, time.dayOfWeek);
+}
