a361de401941afeba050ee68652cbf8f84731363
NatsuDrag9 <rohitimandi9@zoho.com>
2023-08-25 16:36:28 +0200

    Created application entry point

--- ex-alarm-clock-service/main.c
+++ ex-alarm-clock-service/main.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
+#include "inc/TimeService.h"
+#include "inc/AlarmClockService.h"
 
 int main(int argc, char const *argv[])
 {
     /* code */
+    AlarmClockService_RunCallback(TICKS_OF_HUNDRED_MS_FOR_SECOND);
+    AlarmClockService_RunCallback(TICKS_OF_HUNDRED_MS_FOR_MINUTE);
+    AlarmClockService_RunCallback(TICKS_OF_HUNDRED_MS_FOR_HOUR);
+    AlarmClockService_RunCallback(TICKS_OF_HUNDRED_MS_FOR_DAY);
+    AlarmClockService_RunCallback(TICKS_OF_HUNDRED_MS_FOR_MONTH);
+    AlarmClockService_RunCallback(TICKS_OF_HUNDRED_MS_FOR_YEAR);
     return 0;
 }
