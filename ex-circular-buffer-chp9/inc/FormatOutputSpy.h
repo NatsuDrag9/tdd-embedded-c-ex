e6d895e19f1a9bc1d31e1665389cd8b4451c3f51
NatsuDrag9 <rohitimandi9@zoho.com>
2023-09-16 14:59:06 +0200

    Updated function signature and added block comment

--- ex-circular-buffer-chp9/inc/FormatOutputSpy.h
+++ ex-circular-buffer-chp9/inc/FormatOutputSpy.h
@@ -1,6 +1,6 @@
 #include "./utils.h"
 
-void FormatOutputSpy_Create(int);
+void FormatOutputSpy_Create(int size);
 int FormatOutputSpy(const char* format, ...);
 const char* FormatOutputSpy_GetOutput(void);
 void FormatOutputSpy_Destroy(void);
