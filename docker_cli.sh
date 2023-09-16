f123d8c5f64a235a75eb2d3397cc289f211835f5
NatsuDrag9 <rohitimandi9@zoho.com>
2023-09-16 11:51:22 +0200

    Added Jenkins dockerfile

---
+++ docker_cli.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Downloads and runs the docker:dind image to execute docker commands inside Jenkins nodes
+docker run \
+  --name jenkins-docker \
+  --rm \
+  --detach \
+  --privileged \
+  --network jenkins \
+  --network-alias docker \
+  --env DOCKER_TLS_CERTDIR=/certs \
+  --volume jenkins-docker-certs:/certs/client \
+  --volume jenkins-data:/var/jenkins_home \
+  --publish 2376:2376 \
+  docker:dind \
+  --storage-driver overlay2
+
+# Builds the dockerfile containing the blueocean plugin 
+# located in root directory of the project
+# docker build -t myjenkins-blueocean:2.414.1-1 .
+
+# Runs the docker image containing blueocean plugin as a container
+docker run --name jenkins-blueocean --restart=on-failure --detach \
+  --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
+  --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 \
+  --volume jenkins-data:/var/jenkins_home \
+  --volume jenkins-docker-certs:/certs/client:ro \
+  --publish 8080:8080 --publish 50000:50000 myjenkins-blueocean:2.414.1-1
