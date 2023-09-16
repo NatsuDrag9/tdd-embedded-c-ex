6911594c1ca4d6c2358188ebecd0c6d83442163a
NatsuDrag9 <rohitimandi9@zoho.com>
2023-09-16 15:27:04 +0200

    Added include guards

--- ex-circular-buffer-chp9/inc/FormatOutputSpy.h
+++ ex-circular-buffer-chp9/inc/FormatOutputSpy.h
@@ -1,6 +1,12 @@
+#ifndef FORMAT_OUTPUT_SPY_H
+#define FORMAT_OUTPUT_SPY_H
+
 #include "./utils.h"
 
+/* APIs */
 void FormatOutputSpy_Create(int size);
 int FormatOutputSpy(const char* format, ...);
 const char* FormatOutputSpy_GetOutput(void);
 void FormatOutputSpy_Destroy(void);
+
+#endif /* FORMAT_OUTPUT_SPY_H */
--- ex-circular-buffer-chp9/inc/utils.h
+++ ex-circular-buffer-chp9/inc/utils.h
@@ -1,1 +1,6 @@
+#ifndef UTILS_H
+#define UTILS_H
+
 extern int (*FormatOutput)(const char*, ...);
+
+#endif /* UTILS_H */
