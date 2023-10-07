8acd8ca3b03fbe79517e4adf62c36ea0e06d9aa5
NatsuDrag9 <rohitimandi9@zoho.com>
2023-10-07 19:25:55 +0200

    Fixed multiple definition error

--- ex-erase-suspend-resume-flowchart/src/io.c
+++ ex-erase-suspend-resume-flowchart/src/io.c
@@ -1,13 +1,13 @@
 #include "../inc/io.h"
 
 /* Implementation of IO_Write */
-void IO_Write(ioAddress addrOffset, ioData data){
-    ioData* ptr = NULL;
-    *(ptr + addrOffset) = data;
-}
+// void IO_Write(ioAddress addrOffset, ioData data){
+//     ioData* ptr = NULL;
+//     *(ptr + addrOffset) = data;
+// }
 
-/* Implementation of IO_Read */
-ioData IO_Read(ioAddress addrOffset){
-    ioData* ptr = NULL;
-    return *(ptr + addrOffset);
-}
+// /* Implementation of IO_Read */
+// ioData IO_Read(ioAddress addrOffset){
+//     ioData* ptr = NULL;
+//     return *(ptr + addrOffset);
+// }
